e2c4d679e39c3858a4c81b948a024627308e2c87
jOh <j0h@example.com>
2020-08-07 17:21:13 +0530

    made rough diffusion

--- Encryption/Encrypt.py
+++ Encryption/Encrypt.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from Encryption.FaceDetection import Detection
@@ -13,7 +14,11 @@ class FileEncryption:
         self.filepath = "Images\\"+filename
         self.l = None
         self.x=None
+        #donot make obj member(change)
         self.obj=None
+        # self.xs=None
+        # self.sigma=None
+        #self.n_seg=None
 
 
 
@@ -36,8 +41,12 @@ class FileEncryption:
         for cordinate in cordinates:
             self.modifyFace(cordinate, pixelsNew)
         #removing the confusion
-        # for cordinate in cordinates:
-        #     self.modifyFace(cordinate, pixelsNew)
+        for cordinate in cordinates:
+            self.modifyFace(cordinate, pixelsNew)
+        for cordinate in cordinates:
+            pixelsNew=self.diffusion(cordinate,pixelsNew)
+        for cordinate in cordinates:
+            pixelsNew=self.diffusion(cordinate,pixelsNew)
         im.close()
         img.show()
         path = "Images\\encrypted.png"
@@ -67,14 +76,43 @@ class FileEncryption:
                 ind+=1
 
 
-    def diffusion(self):
+    def diffusion(self,cordinate,pixelsNew):
         print("diffusion",end="\n")
+        size=cordinate[2]*cordinate[3]
+        # no of segments into which face is divided
+        n_seg = random.randint(10,size/100)
+        # no of pixels in one segment
+        spix = int(math.ceil(size/n_seg))
+        xs = random.randint(0, 1000000) / 1000000
+        sigma = random.randint(870000, 1000000) / 1000000
+        pix=list(pixelsNew)
+        for i in range(n_seg):
+            val=xs
+            oc={}
+            ind=0
+            for j in range(spix):
+                val=sigma*math.sin(math.pi*val)
+                position=round(val*(spix-1))
+                indx=(spix*i+position)/cordinate[2]
+                indy=(spix*i+position)%cordinate[2]
+                if (indx,indy) in oc:
+                    continue
+                else:
+                    oc.add((indx,indy))
+                    l=(spix*i+ind)/cordinate[2]
+                    r=(spix*i+ind)%cordinate[2]
+                    pix[indx,indy]=pixelsNew[l,r]
+                    ind=ind+1
+        return pix
+
+
 
     def getInitialValues(self):
         # print("getIntialValues",end=' ')
         # select a random value between a and b inclusive both
         self.l=random.randint(3560000,4000000)/1000000
         self.x=random.randint(0,1000000)/1000000
+
 
 obj = FileEncryption('image2.png')
 obj.confusion()
