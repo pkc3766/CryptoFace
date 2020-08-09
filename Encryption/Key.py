aafc741a2a36422421dd90202542a1b42cbabf46
jOh <j0h@example.com>
2020-08-09 16:26:56 +0530

    need to handle errors

---
+++ Encryption/Key.py
@@ -0,0 +1,9 @@
+class keyFile:
+    def __init__(self):
+        # self.l = None
+        # self.x = None
+        # self.xs = None
+        # self.sigma = None
+        # self.n_seg = None
+        self.constants=[]
+        self.cordinates = None
